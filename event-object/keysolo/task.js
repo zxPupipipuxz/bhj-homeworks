a88105490cbe1a243f0b87a90fdc4b4a2f151882
Pupipipu <zxkirillxz@gmail.com>
2026-01-19 15:28:57 +0700

    соло готово

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,6 +17,14 @@ class Game {
   }
 
   registerEvents() {
+    document.addEventListener('keydown', (event) =>{
+      const eventKey = event.key.toLowerCase();
+      if (this.currentSymbol.textContent.toLowerCase() === eventKey) {
+        this.success();
+      }else{
+        this.fail()
+      }
+    })
     /*
       TODO:
       Написать обработчик события, который откликается
