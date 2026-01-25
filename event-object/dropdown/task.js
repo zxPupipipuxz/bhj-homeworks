17ac0303d325a92bf6504e3bab95a6ee4c585346
Pupipipu <zxkirillxz@gmail.com>
2026-01-25 16:28:08 +0700

    dropdown ready

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,15 +1,17 @@
-const menuName = document.querySelector('.dropdown__value');
-const menuList = document.querySelector('.dropdown__list');
+const menuName = document.querySelectorAll('.dropdown__value');
+const menuList = document.querySelectorAll('.dropdown__list');
 const menuItems = Array.from(document.querySelectorAll('.dropdown__item'));
 
-menuName.addEventListener('click', () => {
-    menuList.classList.toggle('dropdown__list_active');
-});
+for (let menu of menuName) {
+    menu.addEventListener('click', () => {
+        menu.nextElementSibling.classList.toggle('dropdown__list_active');
+    });
+}
 
 for (let item of menuItems) {
-    item.addEventListener('click', function(event) {
+    item.addEventListener('click', function (event) {
         event.preventDefault();
-        menuName.textContent = event.currentTarget.textContent;
-        menuList.classList.remove('dropdown__list_active');
+        this.parentElement.previousElementSibling.textContent = event.currentTarget.textContent;
+        this.parentElement.classList.remove('dropdown__list_active');
     })
 }
